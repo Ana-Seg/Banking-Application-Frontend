cfb0df3cd297b3119e71af57afd2b4f0b8398f94
Ana Segoviano <segoviano.a@outlook.com>
2024-11-13 21:32:51 -0600

    Test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,22 +4,22 @@ import bank from "../assets/bank.png";
 
 const Home = () => {
   return (
-    <div className='p-4 max-w-[1200px] mx-auto mt-16 bg-gray-100 rounded-lg shadow-lg'>
+    <div className='p-4 max-w-[1200px] mx-auto mt-10 bg-gray-100 rounded-lg shadow-lg'>
       <div className='text-center'>
-        <h1 className='text-4xl sm:text-5xl lg:text-6xl my-8 font-bold'>
+        <h1 className='text-4xl sm:text-5xl lg:text-6xl mb-6 font-bold'>
           Welcome to <span className='text-blue-800'>BadBank</span>
         </h1>
-        <p className='text-lg sm:text-xl mb-8'>
+        <p className='text-lg sm:text-xl mb-4'>
           Your trusted partner for all your banking needs. Experience seamless banking with us!
         </p>
         <Link to="/register" className='bg-blue-800 text-white text-lg py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300'>
           Get Started
         </Link>
       </div>
-      <div className='mt-8'>
+      <div className='mt-4'>
         <img src={bank} alt="bank" className="rounded-2xl h-auto w-11/12 sm:w-3/4 max-w-[400px] mx-auto object-cover shadow-md" />
       </div>
-      <div className='mt-8 text-center'>
+      <div className='mt-4 text-center'>
         <h2 className='text-2xl sm:text-3xl lg:text-4xl font-semibold mb-4'>Why Choose Us?</h2>
         <ul className='list-disc list-inside text-base sm:text-lg'>
           <li>Secure and Safe Transactions</li>
