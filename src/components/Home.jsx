d6c34b12897894dacfd5360d6c3f41913c23c299
Ana Segoviano <segoviano.a@outlook.com>
2024-11-13 21:44:33 -0600

    Test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className='p-4 max-w-[1200px] mx-auto mt-16 bg-gray-100 rounded-lg shadow-lg'>
       <div className='text-center'>
-        <h1 className='text-4xl sm:text-5xl lg:text-6xl mb-4 font-bold'>
+        <h1 className='text-4xl sm:text-5xl lg:text-4xl mb-4 mt-4 font-bold'>
           Welcome to <span className='text-blue-800'>BadBank</span>
         </h1>
         <p className='text-lg sm:text-xl mb-4'>
@@ -20,8 +20,8 @@ const Home = () => {
         <img src={bank} alt="bank" className="rounded-2xl h-auto w-11/12 sm:w-3/4 max-w-[400px] mx-auto object-cover shadow-md" />
       </div>
       <div className='mt-4 text-center'>
-        <h2 className='text-2xl sm:text-3xl lg:text-4xl font-semibold mb-4'>Why Choose Us?</h2>
-        <ul className='list-disc list-inside text-base sm:text-lg'>
+        <h2 className='text-2xl sm:text-3xl lg:text-4xl font-semibold mt-2 mb-4'>Why Choose Us?</h2>
+        <ul className='list-disc list-inside text-base sm:text-lg mt-2'>
           <li>Secure and Safe Transactions</li>
            <li>24/7 Customer Support</li>
            <li>Easy-to-use Mobile App</li>
